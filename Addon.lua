f241f8b9bd7eb1da8d3b8365b9ff224b1d66378d
keremdincer <b.keremdincer@gmail.com>
2015-07-29 16:59:41 +0300

    Level Up Addon
    #3

--- Addon.lua
+++ Addon.lua
@@ -1,1 +1,10 @@
-message('Hello World!')
+message("Yobbyz")
+
+local my_event = CreateFrame("Frame")
+my_event:RegisterEvent("PLAYER_LEVEL_UP")
+
+my_event:SetScript("OnEvent",
+	function( self, event, ... )
+		local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+		print('Aferin lan level' .. arg1 .. 'oldun!' .. UnitName("Player"))
+	end)
