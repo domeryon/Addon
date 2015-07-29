23f5c54c2819f4ea6b5d94d32d94244ec1019d5e
keremdincer <b.keremdincer@gmail.com>
2015-07-29 19:26:35 +0300

    Fix

--- Addon.lua
+++ Addon.lua
@@ -9,17 +9,17 @@ my_event:SetScript("OnEvent",
 		print('Aferin lan level ' .. arg1 .. ' oldun!' .. UnitName("Player"))
 	end)
 
-local f = CreateFrame("Frame",nil,UIParent)
-f:SetFrameStrata("BACKGROUND")
-f:SetWidth(128) -- Set these to whatever height/width is needed 
-f:SetHeight(64) -- for your Texture
+-- local f = CreateFrame("Frame",nil,UIParent)
+-- f:SetFrameStrata("BACKGROUND")
+-- f:SetWidth(128) -- Set these to whatever height/width is needed 
+-- f:SetHeight(64) -- for your Texture
 
-local t = f:CreateTexture(nil,"BACKGROUND")
-t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
-t:SetAllPoints(f)
-f.texture = t
+-- local t = f:CreateTexture(nil,"BACKGROUND")
+-- t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
+-- t:SetAllPoints(f)
+-- f.texture = t
 
-f:SetPoint("CENTER",0,0)
-f:Show()
+-- f:SetPoint("CENTER",0,0)
+-- f:Show()
 
 local posX, posY = GetPlayerMapPosition("player");
